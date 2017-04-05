7b7aa5c046dea0351a8e13c10c32c20f322a3516
douglas.silveira <oliveira.douglas93@gmail.com>
2017-04-05 14:32:05 -0300

    Cesar lindo

--- ClubApp/admin.py
+++ ClubApp/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import team, players, club_history
+from .models import team, players, club_history,position
 
 
 # Register your models here.
 admin.site.register(team)
 admin.site.register(players)
 admin.site.register(club_history)
+admin.site.register(position)
 
 
--- ClubApp/models.py
+++ ClubApp/models.py
@@ -6,25 +6,27 @@ from django.db import models
 
 foot = ['Destro','Canhoto']
 
-class  team(models.Model):
+class team(models.Model):
     name = models.TextField(max_length=60)
     def __str__(self):
         return self.name
 
 
-#class position(models.Model):
-#    name = models.TextField(20)
+class position(models.Model):
+    name = models.TextField(max_length=20, verbose_name='Posicao')
+    def __str__(self):
+        return self.name
 
 
 class  players(models.Model):
     name = models.TextField(max_length=150)
-    age = models.IntegerField()
+    age = models.DateField(default=01/01/1900)
     foot = models.TextField(max_length=30)
     #hoto = models.
     role = models.TextField(max_length=50)
     team = models.ForeignKey(team)
     nickname = models.TextField(max_length=60, default='Dodo')
-    position = models.TextField(max_length=20,default='Meia')
+    position = models.ForeignKey(position)
     words = models.TextField(max_length=500, verbose_name='Lema', default='Eu sou o Dougras')
     def __str__(self):
         return self.name
@@ -34,3 +36,9 @@ class  players(models.Model):
 
 class club_history(models.Model):
     history = models.TextField(max_length=2000)
+    def __str__(self):
+        return self.history
+
+
+
+
